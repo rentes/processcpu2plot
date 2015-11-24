a489a317e6b18a4352e9fcfdce444885d1c8d85d
Miguel Rentes <miguel.rentes@gmail.com>
2015-11-24 17:03:14 +0000

    Refactors solution (PEP-8 and Pylint compliance)

--- processcpu2plot.py
+++ processcpu2plot.py
@@ -27,13 +27,15 @@ import sys
 USAGE = "usage: processcpu2plot.py <process-name> " +\
         "<iterations> <interval>\n" +\
         "exiting."
-INCORRECT_NUMBER_OF_PARAMETERS_FOUND = "incorrect number of parameters found.\n" + USAGE
-
+INCORRECT_NUMBER_OF_PARAMETERS_FOUND = "incorrect number of parameters.\n" +\
+                                       USAGE
 NO_PROCESS_FOUND = "process not found. please check input parameters. " +\
                    "process name has to exactly match, e.g. chrome.exe " +\
                    "not chrome.EXE nor Chrome.exe"
-INCORRECT_ITERATIONS_FOUND = "incorrect iterations parameter. It must be an integer.\n" + USAGE
-INCORRECT_INTERVAL_FOUND = "incorrect interval parameter. It must be a float.\n" + USAGE
+INCORRECT_ITERATIONS_FOUND = "incorrect iterations parameter. " +\
+                             "It must be an integer.\n" + USAGE
+INCORRECT_INTERVAL_FOUND = "incorrect interval parameter. " +\
+                           "It must be a float.\n" + USAGE
 
 
 class ProcessCPU2Plot:
@@ -44,6 +46,8 @@ class ProcessCPU2Plot:
         self.process = process
         self.iterations = iterations
         self.interval = interval
+        self.pids = []
+        self.values = []
 
     def validate_parameters(self):
         """ Validates if there are parameters entered on the command line
@@ -100,40 +104,35 @@ class ProcessCPU2Plot:
                 pids.append(psutil.Process(_process.pid))
         return pids
 
-    def process_cpu_values(self, _process, _iterations, _interval):
+    def process_cpu_values(self):
         """ Gets the CPU % values from the running process PIDs
-        :param _process: The list of process PIDs
-        :param _iterations: How many iterations will be to obtain CPU % values
-        :param _interval: How long each iteration will last in seconds
         :return: A list with the CPU % for each running process PID
         """
-        values_array = [[0 for x in range(_iterations)]
-                        for x in range(len(_process))]
-        for iteration in range(_iterations):
-            for process_index in range(len(_process)):
+        values_array = [[0 for x in range(self.iterations)]
+                        for x in range(len(self.pids))]
+        for iteration in range(self.iterations):
+            for process_index in range(len(self.pids)):
                 try:
                     values_array[process_index][iteration] = \
-                        _process[process_index].cpu_percent(_interval)
+                        self.pids[process_index].cpu_percent(self.interval)
                 except psutil.NoSuchProcess:
-                    print('process with ID ' + str(_process[process_index].pid) +
+                    print('process with ID ' +
+                          str(self.pids[process_index].pid) +
                           ' no longer running. setting cpu % to 0.0')
                     values_array[process_index][iteration] = 0.0
         return values_array
 
-    def plot(self, _values, _pids, _iterations, _interval):
+    def plot(self):
         """ Uses the matplotlib to create a line graphic plot
-        :param _values: the list of CPU % values
-        :param _pids: the list of PIDs (for the label on the graphic plot)
-        :param _iterations: How many iterations will be
-        :param _interval: How long each iteration lasts
         """
-        for pid_value in range(len(_values)):
-            plt.plot(_values[pid_value], label=str(_pids[pid_value].pid))
+        for pid_value in range(len(self.values)):
+            plt.plot(self.values[pid_value],
+                     label=str(self.pids[pid_value].pid))
         plt.ylabel(sys.argv[1] + ' CPU %')
-        plt.xlabel('time: ' + str(_iterations) + ' iterations of ' +
-                   str(_interval) + 's each')
-        plt.axis([0, _iterations, 0, 100])
-        if len(_pids) == 1:
+        plt.xlabel('time: ' + str(self.iterations) + ' iterations of ' +
+                   str(self.interval) + 's each')
+        plt.axis([0, self.iterations, 0, 100])
+        if len(self.pids) == 1:
             plt.legend(loc=1, ncol=2, shadow=True, title="PID", fancybox=True)
         else:
             plt.legend(loc=1, ncol=2, shadow=True, title="PIDs", fancybox=True)
@@ -141,13 +140,12 @@ class ProcessCPU2Plot:
 
 
 if __name__ == "__main__":
-    number_of_parameters = len(sys.argv) - 1
-    if number_of_parameters < 3:
+    NUMBER_OF_PARAMETERS = len(sys.argv) - 1
+    if NUMBER_OF_PARAMETERS < 3:
         print(INCORRECT_NUMBER_OF_PARAMETERS_FOUND)
         exit(-1)
-    proc = ProcessCPU2Plot(sys.argv[1], sys.argv[2], sys.argv[3])
-    proc.validate_parameters()
-    pids = proc.process_pid()
-    values = proc.process_cpu_values(pids, proc.iterations, proc.interval)
-    proc.plot(values, pids, proc.iterations, proc.interval)
-
+    PROC = ProcessCPU2Plot(sys.argv[1], sys.argv[2], sys.argv[3])
+    PROC.validate_parameters()
+    PROC.pids = PROC.process_pid()
+    PROC.values = PROC.process_cpu_values()
+    PROC.plot()
